db353e0e49f0366fa51d9d05b3ab1fda0ac676c3
Domonkos Sasi <domonkos.sasi@stud.uni-corvinus.hu>
2022-09-30 14:09:04 +0200

    User class, form design and resource for names

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //összeűzi a FirstName és LastName property-ket
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName
+                    );
+            }
+        }
+    }
+}
