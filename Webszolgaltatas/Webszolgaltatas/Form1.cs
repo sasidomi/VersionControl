9429fe986b4d3c7567b14f0fd85b8b00505ddacc
Domonkos Sasi <domonkos.sasi@stud.uni-corvinus.hu>
2022-10-21 14:02:48 +0200

    1, 2, 3, 4

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgaltatas.Entities;
+using Webszolgaltatas.MnbServiceReference;
 
 namespace Webszolgaltatas
 {
@@ -15,6 +17,29 @@ namespace Webszolgaltatas
         public Form1()
         {
             InitializeComponent();
+            GetEuroExchangeRate();
+            dataGridView1.DataSource = Rates;
         }
+
+        void GetEuroExchangeRate()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            //Console.Write(result);
+        }
+
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
     }
 }
