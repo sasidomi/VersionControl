bd0540e9c48a153981ffd1cc3c97d40b37188021
sasmn <0.3sasi@gmail.com>
2022-10-24 14:01:55 +0200

    4

---
+++ FejlesztesiMintak/FejlesztesiMintak/Abstractions/Toy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FejlesztesiMintak.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            //throw new NotImplementedException();
+            DrawImage(e.Graphics);
+        }
+
+        //így ez a függvény minden osztályban kötelező lesz
+        protected abstract void DrawImage(Graphics g);
+
+        //virtual: felül lehet írni a leszármaztatott osztályokban, de önmagában is használható
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
