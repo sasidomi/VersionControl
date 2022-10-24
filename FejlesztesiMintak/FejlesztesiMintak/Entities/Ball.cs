802a0711476fad74f7d50cc3ea5b02daa5fc5e9f
sasmn <0.3sasi@gmail.com>
2022-10-24 13:48:26 +0200

    3

--- FejlesztesiMintak/FejlesztesiMintak/Entities/Ball.cs
+++ FejlesztesiMintak/FejlesztesiMintak/Entities/Ball.cs
@@ -8,7 +8,7 @@ using System.Drawing;
 
 namespace FejlesztesiMintak.Entities
 {
-    class Ball : Label
+    public class Ball : Label
     {
         public Ball()
         {
--- FejlesztesiMintak/FejlesztesiMintak/Form1.cs
+++ FejlesztesiMintak/FejlesztesiMintak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FejlesztesiMintak.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,48 @@ namespace FejlesztesiMintak
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        //Inconsistent accessibility: property type 'type' is less accessible than property 'property'
+        //megoldás: a Ball és BallFactory class-okat is public-ra rakni az Entities-ben
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            //automatikusan aktív: Design -> Properties -> Enabled: true
+            Ball b = Factory.CreateNew();
+            _balls.Add(b);
+            b.Left = -b.Width;
+            mainPanel.Controls.Add(b);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            //automatikusan aktív: Design -> Properties -> Enabled: true
+            var mostRightBall = 0;
+            foreach (Ball b in _balls)
+            {
+                b.MoveBall();
+
+                if (b.Left > mostRightBall)
+                {
+                    mostRightBall = b.Left;
+                }
+            }
+            if (mostRightBall > 1000)
+            {
+                var firstBall = _balls[0];
+                _balls.Remove(firstBall);
+                mainPanel.Controls.Remove(firstBall);
+            }
         }
     }
 }
