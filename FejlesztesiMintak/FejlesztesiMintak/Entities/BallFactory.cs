6e7be3b302cfc1e56b3b48d3e264f775957db09e
sasmn <0.3sasi@gmail.com>
2022-10-24 13:32:02 +0200

    2

---
+++ FejlesztesiMintak/FejlesztesiMintak/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FejlesztesiMintak.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            Ball b = new Ball();
+            return b;
+        }
+    }
+}
