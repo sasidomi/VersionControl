69f3f41a43831dd69f2e733e7584edc000767b42
Domonkos Sasi <domonkos.sasi@stud.uni-corvinus.hu>
2022-11-11 14:22:35 +0100

    5

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -7,20 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaR.Entities;
 
 namespace VaR
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+
+        PortfolioEntities context = new PortfolioEntities();
+
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
         }
 
-        List<Tick> Ticks;
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
-        PortfolioEntities context = new PortfolioEntities();
+            dataGridView2.DataSource = Portfolio;
+        }
     }
 }
