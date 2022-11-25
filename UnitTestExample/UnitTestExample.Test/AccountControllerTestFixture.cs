a829fac816b5f0fcfe9f8a0d54c396d85513d125
Domonkos Sasi <domonkos.sasi@stud.uni-corvinus.hu>
2022-11-25 14:05:55 +0100

    5

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -10,14 +10,40 @@ namespace UnitTestExample.Entities
 {
     public class AccountControllerTestFixture
     {
-        [Test]
-        public void TEstValidateEmail(string email, bool expectedResult)
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
             var accountController = new AccountController();
 
             // Act
             var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("nincsszam", false),
+            TestCase("NINCSKISBETU", false),
+            TestCase("nincsnagybetu", false),
+            TestCase("rovid", false),
+            TestCase("JoJelszo5", true)
+            ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
