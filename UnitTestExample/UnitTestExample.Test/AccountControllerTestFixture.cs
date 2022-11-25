f8a6e8f3bb8da7c6ead64e4fcf21a7eb5bb544ac
Domonkos Sasi <domonkos.sasi@stud.uni-corvinus.hu>
2022-11-25 13:55:56 +0100

    4

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Entities
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TEstValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
