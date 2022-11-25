a02cbd8d295b64109ebc2fe6d636a8d086c24dec
Domonkos Sasi <domonkos.sasi@stud.uni-corvinus.hu>
2022-11-25 13:37:34 +0100

    3

---
+++ UnitTestExample/UnitTestExample/Entities/AccountControllerTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Entities
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TEstValidateEmail(string email, bool expectedResult) { }
+    }
+}
